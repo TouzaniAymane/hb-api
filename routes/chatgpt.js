2b54863c69a72ba18756be5914374292ee01b841
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-07-03 22:31:37 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑
    Signed-off-by: Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>

--- routes/chatgpt.js
+++ routes/chatgpt.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   const inputText = req.query.text;
   const senderName = req.query.name || ''; 
   const prompt = req.query.prompt || ''; 
-  const lenguaje = req.query.lenguaje || 'ar'; 
+  const lenguaje = req.query.lenguaje || 'en'; 
    try {
     if (!inputText) {
       const errorResponse = {
